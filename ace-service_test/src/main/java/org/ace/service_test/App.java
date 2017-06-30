2702370aa19d18644332a5a8f6ddfcedfae93ec8
huangfei <1254209774@qq.com>
2017-06-30 21:04:34 +0800

    version 1.0

---
+++ ace-service_test/src/main/java/org/ace/service_test/App.java
@@ -0,0 +1,13 @@
+package org.ace.service_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
